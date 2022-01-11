9e0f7b7813ed3d19c93e5fc73277e380bd9198b9
cpix42 <ix42@pm.me>
2022-01-11 06:50:44 +0100

    update

--- roles/system/utils/prompt/files/bash_prompt.sh
+++ roles/system/utils/prompt/files/bash_prompt.sh
@@ -1,45 +1,48 @@
 #!/usrv/env bash
 
-### set a colored prompt for server, not to confuse on dev station
-
-### also set a clearer root prompt
-
 # require tput
 if ! which tput &>/dev/null; then
-    echo ERREUR tput introuvable >&2
+    echo "$0" ERREUR tput introuvable >&2
     return
 fi
 
-# black="$(tput setaf 0)"
-red="$(tput setaf 1)"
-#green="$(tput setaf 40)"
-yellow="$(tput setaf 3)"
-# blue="$(tput setaf 4)"
- grey="$(tput setaf 8)"
-# light_blue="$(tput setaf 14)"
+term_type=xterm-256color
+function setaf_with_type() {
+    tput setaf "${1}" -T"${term_type}"
+}
+
+# black="$(setaf_with_type 0)"
+red="$(setaf_with_type 1)"
+#green="$(setaf_with_type 2)"
+light_yellow="$(setaf_with_type 223)"
+# blue="$(setaf_with_type 4)"
+ grey="$(setaf_with_type 252)"
 
 bold="$(tput bold)"
 reset="$(tput sgr0)"
 
 if [ "$(id -u)" == "0" ]; then 
-    PS1="\[$red$bold\]ROOT\[$reset\]@\h\[$reset\] \$ "
+    PS1="\[$red$bold\]ROOT\[$reset\]@\h\[$reset\] \\$ "
 else
-    PS1="\[$yellow\]\u@\h\[$grey\] \$ \[$reset\]"
+    PS1="\[$light_yellow\]\u@\h\[$grey\] \$ \[$reset\]"
 fi
 
-PROMPT_COMMAND="
-    tput setaf 14
+PROMPT_COMMAND='
+    tput setaf 8
+    tput bold
     if (which git && git status) &>/dev/null; then
         basename -s .git $(git config --get remote.origin.url)
-        echo -n ' ↳ '
+        tput setaf 14
+        echo -n ·↳
         git rev-parse --abbrev-ref HEAD
-        echo ' '
-    fi | tr -d '\n'
+        echo -n ·
+    fi | tr -d \\n
 
-    tput setaf 8
+    tput setaf 242
+    echo -n ↓
     pwd
     tput sgr0
-"
+'
 
 export PROMPT_COMMAND
 export PS1
